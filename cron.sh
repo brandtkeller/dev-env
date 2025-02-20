8c84c7a2d59b3a49d819d6e5945256ee7dfa4e72
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-02-19 16:17:17 -0800

    chore(logs): add logging to cron script

--- cron.sh
+++ cron.sh
@@ -5,17 +5,22 @@
 cd /home/dev/repos/dev-env/
 
 # Pull latest changes from git
+echo "Pulling latest changes from git"
 git pull
 
 # Stop existing services
+echo "Stopping existing services"
 docker compose --project-directory compose/ down
 
 # Prune images
+echo "Pruning docker images to prevent cache"
 docker image prune -a -f
 
 # Rebuild images
+echo "Rebuilding container images"
 ./build.sh amd64
 
 # Start new services
+echo "Starting new services"
 docker compose --project-directory compose/ up go-dev -d
 
